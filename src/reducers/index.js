df39440174e37529d973908ac0a40227937f62c0
Adriana Matsufuji <adrianabm@gmail.com>
2016-11-27 15:15:41 +0100

    Create active book reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import BooksReducer from './reducer_books'
+import ActiveBook from './reducer_active_book'
 
+// Any key to the object that we provide to combineReducers ends up as a key on the global state
 const rootReducer = combineReducers({
   books: BooksReducer,
-});
+  activeBook: ActiveBook,
+})
 
 export default rootReducer;
